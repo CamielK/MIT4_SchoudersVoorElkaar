492efd215b2d88c94c2e570ac778fd2236fdb4ae
Camiel <camielk@live.nl>
2016-06-08 18:47:00 +0200

    Added add article and add comment methods to API

--- API/_class/article.php
+++ API/_class/article.php
@@ -27,9 +27,42 @@ class article {
             $articleInfo['error'] = 'Error getting information from database.';
         }
     
+        //close connection
+        $db->closeConnection();
+    
         //return article json
         return json_encode($articleInfo);
         
+    }
+    
+    
+    //return a list of articles that match the given searchstring
+    public function addArticle($articleJson) {
+    	
+        //call to database connection
+        include_once('dbConnection.php');
+        $db = new databaseConnection();
+        
+        //form query
+        $query = "
+            INSERT INTO `sve`.`Article` (`id`, `author_id`, `title`, `content`, `tags`, `category`, `average_rating`, `total_ratings`, `created_at`, `published_at`, `updated_at`) 
+            VALUES (NULL, '".$articleJson['author_id']."', '".$articleJson['title']."', '".$articleJson['content']."', '".$articleJson['tags']."', '".$articleJson['category']."', '0', '0', '".$articleJson['created_at']."', '".$articleJson['published_at']."', '".$articleJson['updated_at']."');";
+        
+        //do query
+        $result = $db->queryDatabase($query);
+
+        //check result
+        if ($result->num_rows > 0) {
+        	$response['success'] = 'Article was added.';
+        } else {
+            $response['error'] = 'Error adding article.';
+        }
+        
+        //close connection
+        $db->closeConnection();
+    
+        //return result
+        return json_encode($response);
     }
     
     
@@ -63,6 +96,9 @@ class article {
         } else {
             $articleList['error'] = 'Did not find any resources matching the search string.';
         }
+        
+        //close connection
+        $db->closeConnection();
     
         //return article json
         return json_encode($articleList);
--- API/_class/comment.php
+++ API/_class/comment.php
@@ -68,6 +68,36 @@ class comment {
         return json_encode($commentList);
         
     }
+    
+    
+    //add the given comment to the database
+    public function addComment($commentJson) {
+    	
+        //call to database connection
+        include_once('dbConnection.php');
+        $db = new databaseConnection();
+        
+        //form query
+        $query = "
+            INSERT INTO `sve`.`Comment` (`id`, `article_id`, `user_id`, `comment_text`, `created_at`, `updated_at`) 
+            VALUES (NULL, '".$commentJson['article_id']."', '".$commentJson['user_id']."', '".$commentJson['comment_text']."', '".$commentJson['created_at']."', '".$commentJson['created_at']."');";
+        
+        //do query
+        $result = $db->queryDatabase($query);
+
+        //check result
+        if ($result->num_rows > 0) {
+        	$response['success'] = 'Comment was added.';
+        } else {
+            $response['error'] = 'Error adding comment.';
+        }
+        
+        //close connection
+        $db->closeConnection();
+    
+        //return result
+        return json_encode($response);
+    }
         
 }
 
